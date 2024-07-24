0416bee94e4333ee6ecd581c3514884c7064f540
peter <petermsouzajr@gmail.com>
2024-07-24 12:28:17 -0700

    initial

---
+++ README.md
@@ -0,0 +1,15 @@
+# Decibel Tribe
+
+## Stay Human
+
+to install dependencies
+`npm install --legacy-peer-deps`
+
+view local prisma db
+`npm prisma studio`
+
+to send new db model info to the db
+`npm prisma db push`
+
+to run local dev
+`npm run dev`
---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,70 @@
+import loginImage from "@/assets/login-image.jpg";
+import { Metadata } from "next";
+import Image from "next/image";
+import Link from "next/link";
+import GoogleSignInButton from "./google/GoogleSignInButton";
+import LoginForm from "./LoginForm";
+
+export const metadata: Metadata = {
+  title: "Login",
+};
+
+export default function Page() {
+  return (
+    <main className="flex h-screen items-center justify-center p-5">
+      <div className="">
+        <h1 className="text-center text-6xl font-bold">Decibel Tribe</h1>
+      </div>
+      <div className="flex h-full max-h-[40rem] w-full max-w-[64rem] overflow-hidden rounded-2xl bg-card shadow-2xl">
+        <div className="w-full space-y-10 overflow-y-auto p-10 md:w-1/2">
+          <h1 className="text-center text-2xl font-bold">Login</h1>
+          <div className="space-y-5">
+            <GoogleSignInButton />
+            <div className="flex items-center gap-3">
+              <div className="h-px flex-1 bg-muted" />
+              <span>OR</span>
+              <div className="h-px flex-1 bg-muted" />
+            </div>
+            <LoginForm />
+            <Link href="/signup" className="block text-center hover:underline">
+              Don&apos;t have an account? Sign up
+            </Link>
+          </div>
+        </div>
+        <Image
+          src={loginImage}
+          alt=""
+          className="hidden w-1/2 object-cover md:block"
+        />
+      </div>
+    </main>
+  );
+}
+
+// <main className="flex h-screen flex-col items-center justify-center p-5 space-y-5">
+//   <div className="w-full text-center">
+//     <h1 className="text-6xl font-bold">Decibel Tribe</h1>
+//   </div>
+//   <div className="flex h-full max-h-[40rem] w-full max-w-[64rem] overflow-hidden rounded-2xl bg-card shadow-2xl">
+//     <div className="w-full space-y-10 overflow-y-auto p-10 md:w-1/2">
+//       <h1 className="text-center text-2xl font-bold">Login</h1>
+//       <div className="space-y-5">
+//         <GoogleSignInButton />
+//         <div className="flex items-center gap-3">
+//           <div className="h-px flex-1 bg-muted" />
+//           <span>OR</span>
+//           <div className="h-px flex-1 bg-muted" />
+//         </div>
+//         <LoginForm />
+//         <Link href="/signup" className="block text-center hover:underline">
+//           Don&apos;t have an account? Sign up
+//         </Link>
+//       </div>
+//     </div>
+//     <Image
+//       src={loginImage}
+//       alt=""
+//       className="hidden w-1/2 object-cover md:block"
+//     />
+//   </div>
+// </main>
