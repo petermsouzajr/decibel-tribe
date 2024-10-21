318db5bcfa7ce45f446c5c0ce99c7ee26ea211f4
peter <petermsouzajr@gmail.com>
2024-10-21 16:12:01 -0700

    added new url

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     supportFile: "cypress/support/e2e.ts",
-    baseUrl: "http://localhost:3000/",
+    baseUrl: "https://decibeltribe.com/",
     defaultCommandTimeout: 10000,
     pageLoadTimeout: 60000,
   },
--- shadowReportConfig.js
+++ shadowReportConfig.js
@@ -6,7 +6,7 @@ module.exports = {
   //    context('math', () => {
   //      it('can add numbers [C2452][smoke]', () => {
   teamNames: [
-    "baron",
+    "Baron",
     //'wilkins',
     //'canonicus',
   ],
