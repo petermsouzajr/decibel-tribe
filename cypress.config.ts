222da297fd766f94f0bb44d6922c854819c8697f
peter <petermsouzajr@gmail.com>
2024-10-22 15:30:05 -0700

    add package

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     supportFile: "cypress/support/e2e.ts",
-    baseUrl: "https://decibeltribe.com/",
+    baseUrl: "https://www.decibeltribe.com/",
     defaultCommandTimeout: 10000,
     pageLoadTimeout: 60000,
   },
